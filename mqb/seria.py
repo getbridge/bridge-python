e29998d4346ab5c40a41e150ab207fe3ee48feef
Paul Bohm <bohmps@gmail.com>
2011-11-24 02:03:54 -0800

    start resolving

--- mqb/seria.py
+++ mqb/seria.py
@@ -1,8 +1,11 @@
 import uuid
+import weakref
 
 class NowObject(object):
     def __init__(self, parent=None, **kwargs):
-        self.children = {}
+        self.children = weakref.WeakValueDictionary()
+
+        self.is_original = False
 
         self.parent = parent
 
@@ -15,12 +18,14 @@ class NowObject(object):
             if not self.pathchain:
                 name = kwargs.get('name', uuid.uuid4().hex)
                 self.pathchain = [name]
+                self.is_original = True
 
             self.root = self.parent.root
             if name:
                 self.parent.register(self)
     
     def register(self, other):
+        print 'REGISTER SELF', self, 'HAS CHILD', other
         self.children[ other.pathchain[0] ] = other
 
     def __getattr__(self, key):
@@ -30,8 +35,19 @@ class NowObject(object):
         pathchain.append(key)
         return NowObject(parent=self, pathchain=pathchain)
 
+    def fullpath(self):
+        x = []
+        y = self
+        while y:
+            print 'ADDING', y.pathchain
+            x.insert(0, '[' + '.'.join(y.pathchain) + ']' )
+            y = y.parent
+        
+        fp = '.'.join(x)
+        return fp
+
     def __repr__(self):
-        return '<' + self.__class__.__name__ + ' ' + '.'.join(self.pathchain) + '>'
+        return '<' + self.__class__.__name__ + ' PathHistory: ' + self.fullpath() + '>'
     
     def __call__(self, *args, **kwargs):
         wrapped_args = args
@@ -40,6 +56,10 @@ class NowObject(object):
 
     def call(self, pathchain, *args, **kwargs):
         pathchain = list(pathchain)
+
+        if not pathchain:
+            print 'END OF PATHCHAIN', self
+            return self
 
         next = pathchain.pop(0)
 
@@ -52,7 +72,7 @@ class NowObject(object):
             return handler(*args, **kwargs)
         
         raise AttributeError(next)
-
+    
 class ChatRoom(NowObject):
     def send_message(self):
         return 'lala'
@@ -61,19 +81,20 @@ class AuthService(NowObject):
     def handle_login(self, username, password):
         print 'LOGIN'
 
-        return ChatRoom(parent=self)
+        return ChatRoom(self.root)
 
 def main():    
     # Root Object the client interfaces with
     now = NowObject()
-
     default = NowObject(now, name='default')
     auth = AuthService(default, name='auth')
 
-    room = now.default.auth.login(username='enki', password='secret') 
-
+    room = now.default.auth.login(username='enki', password='secret')
+    print now.default.children.items()
     print 'room', room
     print 'SENDMESSAGE', room.send_message()
+    
+    print getattr(now, room.pathchain[0])()
 
 if __name__ == '__main__':
     main()
