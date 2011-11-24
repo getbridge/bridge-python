57bf030bb08e4a5931eafe490a7d41b80bde15d6
Paul Bohm <bohmps@gmail.com>
2011-11-24 03:13:03 -0800

    before move to full chain

--- mqb/seria.py
+++ mqb/seria.py
@@ -1,6 +1,36 @@
 import uuid
 import weakref
 
+class NowPromise(object):
+    def __init__(self):
+        self.cb = None
+        self.result = None
+        self.have_result = False
+    
+    def __call__(self):
+        return self
+
+    def callback(self, cb):
+        self.cb = cb
+        self.check_fire()
+    
+    def set_result(self, result):
+        if isinstance(result, NowPromise):
+            result.callback(self.set_result)
+        else:
+            self.result = result
+            self.have_result = True
+            self.check_fire()
+    
+    def check_fire(self):
+        if self.cb and self.have_result:
+            self.cb(self.result)
+
+    def __getattr__(self, funcname):
+        def callme(*args, **kwargs):
+            self.callback( lambda x: getattr(x, funcname)(*args, **kwargs) )
+        return callme
+
 class NowObject(object):
     def __init__(self, parent=None, **kwargs):
         self.children = weakref.WeakValueDictionary()
@@ -12,6 +42,7 @@ class NowObject(object):
         if not self.parent:
             self.root = self
             self.pathchain = []
+            self.is_original = True
         else:
             self.pathchain = kwargs.get('pathchain', [])
             name = None
@@ -25,7 +56,6 @@ class NowObject(object):
                 self.parent.register(self)
     
     def register(self, other):
-        print 'REGISTER SELF', self, 'HAS CHILD', other
         self.children[ other.pathchain[0] ] = other
 
     def __getattr__(self, key):
@@ -39,7 +69,6 @@ class NowObject(object):
         x = []
         y = self
         while y:
-            print 'ADDING', y.pathchain
             x.insert(0, '[' + '.'.join(y.pathchain) + ']' )
             y = y.parent
         
@@ -50,38 +79,57 @@ class NowObject(object):
         return '<' + self.__class__.__name__ + ' PathHistory: ' + self.fullpath() + '>'
     
     def __call__(self, *args, **kwargs):
-        wrapped_args = args
-        wrapped_kwargs = kwargs
-        return self.root.call(self.pathchain, *wrapped_args, **wrapped_kwargs)
+        return self.mycall(self.pathchain, *args, **kwargs)
+    
+    def mycall(self, pathchain, *args, **kwargs):
+        if not self.is_original:
+            print 'NOT ORIGINAL', self, pathchain
+            return self.parent.mycall(pathchain, *args, **kwargs)
+        else:   
+            print 'ORIGINAL', self, pathchain
+            return self.call(pathchain, *args, **kwargs)
 
     def call(self, pathchain, *args, **kwargs):
         pathchain = list(pathchain)
 
         if not pathchain:
             print 'END OF PATHCHAIN', self
-            return self
+            promise = NowPromise()
+            promise.set_result(self)
+            return promise
 
         next = pathchain.pop(0)
 
         child = self.children.get(next, None)
         if child:
-            return child.call(pathchain, *args, **kwargs)
+            promise = NowPromise()
+            promise.set_result( child.call(pathchain, *args, **kwargs) )
+            return promise
 
         handler = getattr(self, 'handle_' + next)
         if handler:
-            return handler(*args, **kwargs)
+            promise = NowPromise()
+            promise.set_result( handler(*args, **kwargs) )
+            return promise
         
+        print self
         raise AttributeError(next)
     
 class ChatRoom(NowObject):
-    def send_message(self):
-        return 'lala'
+    def send_message(self, message):
+        print 'Hello', message 
 
 class AuthService(NowObject):
     def handle_login(self, username, password):
         print 'LOGIN'
 
         return ChatRoom(self.root)
+    
+    def handle_roominfo(self, room):
+        print 'ROOMINFO', room
+
+def prnt(*args):
+    print 'PRNT', ' '.join( repr(x) for x in args )
 
 def main():    
     # Root Object the client interfaces with
@@ -89,12 +137,18 @@ def main():
     default = NowObject(now, name='default')
     auth = AuthService(default, name='auth')
 
+    print '---'
+
     room = now.default.auth.login(username='enki', password='secret')
-    print now.default.children.items()
+    room.callback(prnt)
+
     print 'room', room
-    print 'SENDMESSAGE', room.send_message()
+    room.send_message('WORLD')
+
+    print '---'
+    print auth.roominfo()
     
-    print getattr(now, room.pathchain[0])()
+    # print getattr(now, room.pathchain[0])()
 
 if __name__ == '__main__':
     main()
