882faafc230e96c2ace8498129b328e2dbe6e64f
Paul Bohm <bohmps@gmail.com>
2011-11-26 15:21:05 -0800

    basic tree resolving

--- mqb/seria.py
+++ mqb/seria.py
@@ -1,5 +1,61 @@
 import uuid
 import weakref
+import functools
+
+class PromiseTreeResolver(object):
+    def __init__(self, tree, callback):
+        self.tree = tree
+        self.callback = callback
+        self.waiting = set()
+        self.fired = False
+
+        self.do_resolve([], self.tree)
+        if not self.waiting and not self.fired:
+            self.callback(self.tree)
+
+    def fulfillPromise(self, data, path=None):
+        if path:
+            self.updateTree(path, data)
+        else:
+            self.tree = data
+        
+        self.waiting.remove(path)
+
+        if not self.waiting:
+            self.fired = True
+            self.callback(self.tree)
+
+    def updateTree(self, origpath, data):
+        path = list(origpath)
+
+        pivot = self.tree
+        while len(path) > 1:
+            pivot = pivot[path.pop(0)]
+        
+        pivot[path.pop(0)] = data
+
+    def registerPromise(self, pivot, path):
+        tpath = tuple(path)
+        self.waiting.add( tpath )
+        pivot.callback( functools.partial(self.fulfillPromise, path=tpath) )
+
+    def do_resolve(self, path, pivot):
+        if isinstance(pivot, dict):
+            for key, value in pivot.items():
+                self.do_resolve(path + [key], value)
+        elif isinstance(pivot, list):
+            for (pos, elem) in enumerate(pivot):
+                self.do_resolve(path + [pos], elem)
+        elif isinstance(pivot, basestring):
+            pass
+        elif isinstance(pivot, int):
+            pass
+        elif isinstance(pivot, float):
+            pass
+        elif isinstance(pivot, NowPromise):
+            self.registerPromise(pivot, path)
+        else:
+            print 'unknown element', pivot
 
 class NowPromise(object):
     def __init__(self):
@@ -31,7 +87,9 @@ class NowPromise(object):
 
     def __getattr__(self, funcname):
         def callme(*args, **kwargs):
-            self.callback( lambda x: getattr(x, funcname)(*args, **kwargs) )
+            promise = NowPromise()
+            self.callback( lambda x: promise.set_result(getattr(x, funcname)(*args, **kwargs)) )
+            return promise
         return callme
 
 class NowObject(object):
@@ -64,6 +122,9 @@ class NowObject(object):
     
     def setup(self):
         pass # abstract
+
+    def callback(self, cb):
+        return cb(self)
 
     def register(self, other):
         self.children[ other.name ] = other
@@ -129,31 +190,56 @@ class ChatRoom(NowObject):
     def send_message(self, message):
         print 'Hello', message 
 
-class AuthService(NowObject):
-    def handle_login(self, username, password):
-        print 'LOGIN', username, password
+class ChatServer(NowObject):
+    def join_room(self, room_name):
+        print 'JOIN', room_name
+        return ChatRoom(self, name=room_name)
 
-        return ChatRoom(self.root)
-    
     def handle_roominfo(self, room):
         print 'ROOMINFO', room
 
+class AuthService(NowObject):
+    def setup(self):
+        self._chatserver = ChatServer(self, name='chat')
+
+    def handle_login(self, username, password):
+        print 'LOGIN', username, password
+        if password == 'secret':
+            return self._chatserver
+        else:
+            return None
+    
 def prnt(*args):
     print 'PRNT', ' '.join( repr(x) for x in args )
 
-def main():    
+def test_basic():    
     # Root Object the client interfaces with
     now = NowObject()
-    default = NowObject(now, name='default')
-    auth = AuthService(default, name='auth')
+    auth = AuthService(now, name='auth')
+    public_chat = ChatServer(now, name='chat')
 
-    room = now.default.auth.login(username='enki', password='secret')
-    room.callback(prnt)
+    now.chat().callback(prnt)
 
-    room.send_message('WORLD')
+    private_chat = now.auth.login(username='enki', password='secret')
+    private_chat.callback(prnt)
+
+    flotype = private_chat.join_room('flotype')
+    print 'GOT', flotype
+    flotype.callback(prnt)
+    flotype.send_message('WORLD')
 
     print '---'
-    auth.roominfo(room)
+    private_chat.roominfo(flotype)
+
+def test_resolver():
+    promise = NowPromise()
+    tree =  {'just': {'yet': ['another', promise,] } }
+    ptree = PromiseTreeResolver(tree, prnt)
+    promise.set_result('foo')
+
+def main():
+    #test_basic()
+    test_resolver()
 
 if __name__ == '__main__':
     main()
