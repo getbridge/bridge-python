c8a92faa3c768758a591c2b251e6a797946579b0
Paul Bohm <bohmps@gmail.com>
2011-12-07 13:03:42 -0800

    talk to js

--- mqb/client.py
+++ mqb/client.py
@@ -2,6 +2,9 @@ import tornado
 from seria import NowObject, NowClient
 import types
 
+def prnt(x):
+    print 'PRNT', x
+
 def main():
     now = NowClient()
     
@@ -9,7 +12,7 @@ def main():
         print 'GOT DATA', data
 
     # now.webpull.fetch_url(url="http://xkcd.com/", callback=got_data)
-    now.webpull.fetch_url(url="http://xkcd.com/")
+    now.hello("http://xkcd.com/", prnt)
 
     ioloop = tornado.ioloop.IOLoop.instance()
     ioloop.start()
