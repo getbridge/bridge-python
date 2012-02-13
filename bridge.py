ae24692708115ecd31082c6b5756e89286f05992
Vedant Kumar <vk@vedantk.com>
2012-02-13 03:21:25 -0800

    temp commit

--- bridge.py
+++ bridge.py
@@ -1,8 +1,13 @@
-# XXX:
-# Figure out what is bridge-internal / protected.
+# XXX
+# package twisted and simplejson
+
+import aux
+import service
+import connection
+import reference
 
 import logging
-from collections import namedtuple, defaultdict
+from collections import defaultdict
 
 from twisted.internet import reactor
 
@@ -14,9 +19,9 @@ class Bridge(object):
 
         self._events = defaultdict(list)
         self._connected = False
-        self._connection = Connection(self)
+        self._connection = connection.Connection(self)
         self._children = {
-            'system': _SystemService(self)
+            'system': service._System(self)
         }
 
         # Client-only.
@@ -25,6 +30,30 @@ class Bridge(object):
         # Server-only.
         self.host = kwargs.get('host', 'localhost')
         self.port = kwargs.get('port', 8090)
+
+    def publish_service(self, name, service, func):
+        if name in self_children:
+            self.log.error('Invalid service name: "%s".', name)
+            return
+
+        service.set_chain(['named', name, name])
+        self.connection.publish_service(name, func)
+        self._children[name] = service
+        return service.get_reference()
+
+    def join_channel(self, name, handler, func):
+        serialize(self, handler)
+
+        pass
+
+    def get_service(self, name, func):
+        pass
+
+    def get_channel(self, name):
+        pass
+
+    def get_client_id(self):
+        pass
 
     def on(self, name, func):
         self._events[name].append(func)
@@ -36,19 +65,22 @@ class Bridge(object):
                 func(*args)
         return self
 
-    def removeEvent(self, name, func):
+    def remove_event(self, name, func):
         if name in self._events:
             self._events[name].remove(func)
         return self
 
-    def onReady(self):
+    def ready(self, func):
+        pass
+
+    def _on_ready(self):
         self.log.info('Handshake complete.')
         if not self.connected:
             self.connected = True
             self.emit('ready')
 
-    def onMessage(self, obj):
-        deserialize(self, obj)
+    def _on_message(self, obj):
+        ref.deserialize(self, obj)
         destination = obj.get('destination', None)
         if destination and type(destination) is Ref:
             chain = destination._getChain()
@@ -56,136 +88,10 @@ class Bridge(object):
         else:
             self.log.warning('No destination in message %s.', obj)
 
-    def execute(self, chain, args):
-        service = self._children[chain[Chain.SERVICE]]
-        func = getattr(service, chain[Chain.METHOD], None)
+    def _execute(self, chain, args):
+        service = self._children[chain[ref.SERVICE]]
+        func = getattr(service, chain[ref.METHOD], None)
         if func:
             func(service, *args)
         else:
             self.log.warning('Specified pathchain does not exist: %s.', chain)
-
-
-
-    def getPathObj(self, chain):
-        # use this pathchain to make a Ref
-        pass
-
-    def ready(self, handler):
-        pass
-
-    def publish_service(self, name, server, callback):
-        pass
-
-    def join_channel(self, name, handler, callback):
-        pass
- 
-class _SystemService(object):
-    def __init__(self, bridge):
-        self.bridge = bridge
-
-    def hook_channel_handler(self, name, handler, callback=None):
-        service_name = handler.pathchain.service
-        service = self.bridge._children[service_name]
-        self.bridge._children['channel:' + name] = service
-        if callback:
-            callback(self.bridge.getChannel(name), name)
-
-    def get_service(self, name, callback):
-        if name in self.bridge._children:
-            callback(self.bridge._children[name])
-        else:
-            callback(None, 'Cannot find service {0}.'.format(name))
-
-    def remote_error(self, msg):
-        self.bridge.log.warn(msg)
-        self.bridge.emit('remoteError', msg)
-
-def deserialize(bridge, obj):
-    if type(obj) is dict:
-        for key, val in obj:
-            if type(val) is dict and 'ref' in val:
-                ref = bridge.getPathObj(val['ref'])
-                obj[key] = ref._setOps(val.get('operations', []))
-            else:
-                deserialize(bridge, obj[key])
-    else:
-        for elt in obj:
-            deserialize(bridge, elt)
-
-class Chain:
-    TYPE = 0
-    ROUTE = 1
-    SERVICE = 2
-    METHOD = 3
-
-class Connection(object):
-    DEFAULT_EXCHANGE = 'T_DEFAULT'
-
-    def __init__(self, bridge):
-        self.bridge = bridge
-        self.establish_connection()
-
-    def reconnect(self):
-        self.bridge.log.info('Attempting reconnect.')
-        if not self.connected and self.interval < 12800:
-            self.establish_connection()
-            self.interval *= 2
-            reactor.callLater(self.interval, self.reconnect)
-
-    def establish_connection(self):
-        self.bridge.log.info('Starting TCP connection (%s:%s).',
-                             self.bridge.host, self.bridge.port)
-        self.sock =
-
-
-
-
-Connection.prototype.establishConnection = function () {
-  var self = this,
-      Bridge = this.Bridge;
-
-  // Select between TCP and SockJS transports
-  if (this.options.tcp) {
-    util.info('Starting TCP connection', this.options.host, this.options.port);
-    this.sock = new TCP(this.options).sock;
-  } else {
-    util.info('Starting SockJS connection');
-    this.sock = new SockJS(this.options.url, this.options.protocols, this.options.sockjs);
-  }
-
-  this.sock.onmessage = function (message) {
-    util.info("clientId and secret received", message.data);
-    var ids = message.data.toString().split('|');
-    self.clientId = ids[0];
-    self.secret = ids[1];
-    self.interval = 400;
-
-    self.sock.onmessage = function(message){
-      try {
-        message = util.parse(message.data);
-        util.info('Received', message);
-        Bridge.onMessage(message);
-      } catch (e) {
-        util.error("Message parsing failed: ", e.message, e.stack);
-      }
-    };
-    Bridge.onReady();
-  };
-
-  this.sock.onopen = function () {
-    util.info("Beginning handshake");
-    var msg = {command: 'CONNECT', data: {session: [self.clientId || 0, self.secret || 0]}};
-    msg = util.stringify(msg);
-    self.sock.send(msg);
-  };
-
-  this.sock.onclose = function () {
-    util.warn("Connection closed");
-    self.connected = false;
-    if (self.options.reconnect) {
-      // do reconnect stuff. start at 100 ms.
-      self.reconnect();
-    }
-  };
-};
-
