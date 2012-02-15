5ef41ae7584556a9a649175e60424d8b05cb0fb0
Vedant Kumar <vk@vedantk.com>
2012-02-14 17:14:00 -0800

    got rid of queue stuff

--- connection.py
+++ connection.py
@@ -6,7 +6,8 @@ DEFAULT_EXCHANGE = 'T_DEFAULT'
 
 class BridgeTCP(TCPServer):
     def handle_stream(self, ios, addr):
-        pass
+
+        self.bridge.log.info('Established connection with %s.' % (addr))
 
 class Connection(object):
     def __init__(self, bridge):
@@ -16,7 +17,7 @@ class Connection(object):
         self.server.listen(bridge.port, bridge.host)
         self.bridge.log.info('Started TCP connection (%s:%s).',
                              self.bridge.host, self.bridge.port)
-
+        
         self.loop.add_callback(bridge.onReady)
         self.loop.start()
 
@@ -25,13 +26,7 @@ class Connection(object):
         if not self.connected and self.interval < 12800:
             self.establish_connection()
             self.interval *= 2
-            reactor.callLater(self.interval, self.reconnect)
-
-    def get_queue_name(self):
-        return 'C_%d' % (self.client_id)
-
-    def get_exchange_name(self):
-        return 'T_%d' % (self.client_id)
+            self.loop.add_timeout(self.interval, self.reconnect)
 
     def send(self, msg):
         pass
