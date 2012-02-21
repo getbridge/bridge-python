0cf4c209360ba91b2a139773d13dd06307ef4733
Vedant Kumar <vk@vedantk.com>
2012-02-21 02:29:04 -0800

    updated client chat example

--- test/chatclient.py
+++ test/chatclient.py
@@ -9,19 +9,24 @@ class MsgHandler(Service):
     def msg(self, name, message):
         print(name + ': ' + message)
 
+class LobbyHandler(object):
+    def __init__(self, name):
+        self.name = name
+        self.lobby = None
+
+    def __call__(self, channel):
+        self.lobby = channel
+
+    def send(self, message):
+        self.lobby.msg(self.name, message)
+
+lobby = LobbyHandler('Vedant')
+
 def start_client():
     bridge.get_service('chatserver', start_chat)
 
 def start_chat(chat):
-    print('Got chat service = ', chat)
-    chat.join('lobby', MsgHandler(), get_channel)
-
-lobby = None
-
-def get_channel(channel):
-    lobby = channel
-
-def send(message):
-    lobby.msg('Foo', message)
+    print('Got chat service.')
+    chat.join('lobby', MsgHandler, lobby)
 
 bridge.ready(start_client)
