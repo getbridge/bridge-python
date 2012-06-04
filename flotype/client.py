d6fce729e1b6a119fe0c0686d818219fe54fcc3e
Steve Wang <steve.john.wang@gmail.com>
2012-06-03 19:52:12 -0700

    Fix client.py to be consistent with the rest of the library.

--- flotype/client.py
+++ flotype/client.py
@@ -3,8 +3,5 @@ from flotype import serializer
 class Client(object):
     def __init__(self, bridge, id):
         self.bridge, self.clientId = bridge, id;
-    def getService(self, svc, cb):
-        self.bridge._connection.sendCommand(
-            'GETOPS', {name: svc,
-                       client: this.clientId,
-                       callback: serializer.serialize(this._bridge,cb)})
+    def get_service(self, svc):
+        return reference.Reference(self, ['client', id, svc])
