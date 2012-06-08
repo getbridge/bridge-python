b377913294397118b32cf0332035afd9751a3b35
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-06-07 18:55:05 -0700

    add temporary SSL certificate. include it in the build process

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.2.0',
     author='Flotype Inc.',
     author_email='team@flotype.com',
-    packages=['flotype'],
+    packages=['flotype', 'flotype.data'],
     url='http://pypi.python.org/pypi/FlotypeBridge/',
     license='LICENSE.txt',
     description='A Python API for the Flotype Bridge service.',
