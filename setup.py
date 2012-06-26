6d7a6e983c67e6317517998edafca11f73842e78
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-06-26 16:23:13 -0700

    add README to manifest; increment patch number

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from distutils.core import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='BridgePython',
-    version='0.2.0',
+    version='0.2.2',
     author='',
     author_email='team@getbridge.com',
     packages=['BridgePython', 'BridgePython.data'],
     url='http://pypi.python.org/pypi/BridgePython/',
     license='LICENSE.txt',
     description='A Python API for the Bridge service.',
-    long_description=open('README.md').read(),
+    long_description=read('README.md'),
     requires=[
         "tornado (>= 2.2)",
     ]
